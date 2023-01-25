7558872a161c8abc32cab0841f6bcf6ec174c0fa
Milner Chen <milnerchen0@gmail.com>
2023-01-25 15:32:54 -0500

    deep_dup complete

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,12 +1,14 @@
 def deep_dup(arr)
-    # return [arr] if arr.is_a?(Array) == false
-    return [] if arr.length == 0
     new_arr = []
-    prev_dup = deep_dup(arr[1..-1])
     arr.each do |ele|
-        new_arr += ele
+        if ele.is_a?(Array)
+            new_arr << deep_dup(ele)
+        else
+            new_arr << ele
+        end
     end
     new_arr
+
 end
 
-p deep_dup([["hello", "hi"], ["goodbye", "bye"]])
+# p deep_dup([["hello", "hi"], ["goodbye", "bye"]])
